c8664e9f3075f3c6f04e6efd35112b02f0d7998a
Oleksandr Timoshyn <o.timoshyn@scalr.com>
2022-09-09 14:49:00 +0300

    update

--- mytest.py
+++ mytest.py
@@ -5,3 +5,19 @@ phrase = "mycustomtext"
 phrase = ' '.join(phrase) #add spaces
 phrase = phrase.replace(" ","-") #replace spaces with dash
 print(phrase[::-1]) #reverse
+
+
+#Correct answer 
+phrase = "mycustomtext"
+
+phrase = ' '.join(phrase).replace(" ","-")
+print(phrase[::-1])
+
+
+# HomeWork#2
+
+import re
+
+phrase = "replace spaces with dash"
+result = re.match("dash", phrase)
+print(result)
