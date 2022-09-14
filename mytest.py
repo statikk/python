d5715981cb9dafc1487cff7cd87b93e9f0d64191
Oleksandr Timoshyn <o.timoshyn@scalr.com>
2022-09-14 22:26:43 +0300

    update

--- mytest.py
+++ mytest.py
@@ -13,11 +13,45 @@ phrase = "mycustomtext"
 phrase = ' '.join(phrase).replace(" ","-")
 print(phrase[::-1])
 
+num_1, num_2 = 5, 7
+print(num_1 + num_2)
+
+
+x = float(5)
+y = float(6)
+z = x + y
+print('Result: ' + str(z))
+
+
+
 
 # HomeWork#2
+"""Reg Exp""""" 
 
 import re
 
 phrase = "replace spaces with dash"
-result = re.match("dash", phrase)
+
+result = re.findall(".", phrase)
 print(result)
+
+import re
+
+phrase = "replace spaces with dash"
+
+result = re.findall("\.", phrase)
+print(result)
+
+#-----------alphanumeric only-----------------------------------------
+import re
+
+phrase = "replace spaces with dash"
+
+result = re.findall("\w", phrase)
+print(result)
+
+#-----------phone numbers-----------------------------------------
+import re
+
+
+#--------------
